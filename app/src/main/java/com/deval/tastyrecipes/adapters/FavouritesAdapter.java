ed4b84fcec2cc2c778310b027e030c3bde2c5613
Mohd Irshad <mohammedirshadm6352@conestogac.on.ca>
2022-08-15 11:33:28 -0400

    "Favourites to Recipe transition added"

--- app/src/main/java/com/deval/tastyrecipes/adapters/FavouritesAdapter.java
+++ app/src/main/java/com/deval/tastyrecipes/adapters/FavouritesAdapter.java
@@ -1,6 +1,7 @@
 package com.deval.tastyrecipes.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +9,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.deval.tastyrecipes.R;
+import com.deval.tastyrecipes.activities.recipe;
 import com.deval.tastyrecipes.models.SearchData;
 import com.squareup.picasso.Picasso;
 
@@ -19,8 +22,11 @@ import java.util.List;
 public class FavouritesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<SearchData> favouriteList;
+    CardView favouriteCard;
+    Context _context;
     public FavouritesAdapter(List<SearchData> listOfRecipes, Context context) {
         super();
+        _context = context;
         favouriteList = listOfRecipes;
     }
 
@@ -28,15 +34,26 @@ public class FavouritesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.favouritecard,parent,false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+        favouriteCard = view.findViewById(R.id.favouriteCard);
+
+        return new ViewHolder(view);
     }
+
+
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
             SearchData recipeObj = favouriteList.get(position);
             ((ViewHolder) holder).fRecipeTitle.setText(String.valueOf(recipeObj.getRecipeTitle()));
             ((ViewHolder) holder).fRecipeCategory.setText(recipeObj.getRecipeCategory());
+            favouriteCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(_context, recipe.class);
+                intent.putExtra("recipeTitle", recipeObj.getRecipeTitle());
+                _context.startActivity(intent);
+            }
+            });
             Picasso.get().load(recipeObj.getRecipeImage()).into(((ViewHolder) holder).fRecipeImage);
 
     }
--- app/src/main/java/com/deval/tastyrecipes/fragments/SearchFragment.java
+++ app/src/main/java/com/deval/tastyrecipes/fragments/SearchFragment.java
@@ -1,6 +1,5 @@
 package com.deval.tastyrecipes.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
